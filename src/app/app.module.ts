ce00e8815f6e24e2a733c6967b9f8bced82924a8
Jean Carlos A <jeanc.developer@outlook.com>
2021-10-10 10:31:06 -0300

    Save minimal changes in tabs Se agrega informacion basica sobre que reaizara cada tabs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import  {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent],
